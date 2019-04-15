18b0c7df39c6a3f34432f92619fb540b22e99833
Rob Bishop <c.sagan.ritual@gmail.com>
2019-04-15 11:56:38 -0700

    Initial

---
+++ Components/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Components
+//
+//  Created by Rob Bishop on 4/15/19.
+//  Copyright © 2019 Boring Software. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
